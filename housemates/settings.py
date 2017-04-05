5fdcd05ddf8acf8cee42225f3c5c8c637d772495
Max Rozen <rozenmd@gmail.com>
2017-04-05 22:47:28 +1000

    Fix timezone, remove all SSL settings

--- housemates/settings.py
+++ housemates/settings.py
@@ -101,7 +101,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1', 'housemates.maxrozen.com']
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Sydney'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
