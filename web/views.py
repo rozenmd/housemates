3bbe5e44ff5e8e74ec52c0494336060624d21f06
Max Rozen <rozenmd@gmail.com>
2017-04-09 16:44:51 +1000

    Implement dashboard, add pandas

--- web/views.py
+++ web/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 from group.models import Group
+from payments.models import Payment
+from bills.models import Bill
 from web.models import MyProfile
 
 
@@ -12,4 +14,12 @@ def dashboard(request, template_name='web/dashboard.html'):
     else:
         current_group = ''
     data = {'current_group': current_group}
+
+    if current_group:
+        bills = Bill.objects.filter(group=current_group)
+        payments = Payment.objects.filter(group=current_group)
+        
+        data['bills'] = bills
+        data['payments'] = payments
+
     return render(request, template_name, data)
