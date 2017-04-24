3293995a61d8ae1e5d25ccf1fd3c0fb78eee0f75
Max Rozen <rozenmd@gmail.com>
2017-04-24 20:48:18 +1000

    Make things shinyyyyy

--- bills/forms.py
+++ bills/forms.py
@@ -1,4 +1,3 @@
-
 from django.forms import ModelForm
 from django import forms
 from bills.models import Bill
@@ -7,7 +6,15 @@ from group.models import GroupMember
 
 class BillForm(ModelForm):
     # group = forms.CharField(label="Household", required=True,disabled=True)
-    # description = forms.CharField(label="Description", required=False)
+    who_paid = forms.ModelChoiceField(required=True, queryset="",
+                                      widget=forms.Select(attrs={'class': 'col-sm-12 col-xs-12'}))
+    who_owes = forms.ModelChoiceField(required=True, queryset="",
+                                      widget=forms.Select(attrs={'class': 'col-sm-12 col-xs-12'}))
+    description = forms.CharField(label="Description", required=False,
+                                  widget=forms.Textarea(attrs={'style': 'height:75px', 'class': 'col-sm-12 col-xs-12'}))
+    quantity_paid = forms.DecimalField(label="Quantity Paid", required=True,
+                                       widget=forms.NumberInput(attrs={'class': 'col-sm-12 col-xs-12'}))
+
     class Meta:
         model = Bill
-        fields = ['who_paid', 'description', 'quantity_paid','who_owes','group']
+        fields = ['who_paid', 'description', 'quantity_paid', 'who_owes', 'group']
--- payments/forms.py
+++ payments/forms.py
@@ -6,8 +6,12 @@ from group.models import GroupMember
 
 
 class PaymentForm(ModelForm):
-    # group = forms.CharField(label="Household", required=True,disabled=True)
-    # description = forms.CharField(label="Description", required=False)
+    from_user = forms.ModelChoiceField(required=True, queryset="",
+                                      widget=forms.Select(attrs={'class': 'col-sm-12 col-xs-12'}))
+    to_user = forms.ModelChoiceField(required=True, queryset="",
+                                      widget=forms.Select(attrs={'class': 'col-sm-12 col-xs-12'}))
+    quantity_paid = forms.DecimalField(label="Quantity Paid", required=True,
+                                       widget=forms.NumberInput(attrs={'class': 'col-sm-12 col-xs-12'}))
     class Meta:
         model = Payment
         fields = ['from_user', 'to_user', 'group','quantity_paid']
